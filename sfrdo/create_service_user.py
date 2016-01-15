4496a7c7336504546cd86620f1a9b13c58bd865b
Fabien Boucher <fabien.boucher@enovance.com>
2016-01-15 11:35:14 +0000

    Adapt sync maintainer for our service user (sfbender)
    - Add a quick script to create the service user
    - Remove the two blacklisted project due to the fixed issue
      on SF related to project started by a 'a'
    
    Change-Id: I8ace6fb031adaca72ddccff963b571aed8f06aa2

---
+++ sfrdo/create_service_user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from pysflib.sfauth import get_cookie
+from sfrdo import config
+
+#  Need to run that before fetching the cookie:
+#  sfmanager --url https://rpmfactory.beta.rdoproject.org --auth admin:xxx \
+#  user create --username sfbender --password 'userpass' --email \
+#  'dev-robot@rpmfactory.beta.rdoproject.org' --fullname \
+#  'Bender RPM Factory' --ssh-key /var/lib/jenkins/.ssh/id_rsa.pub
+
+auth_cookie = {'auth_pubtkt': get_cookie(config.rpmfactory,
+                                         'sfbender', 'userpass')}
