f6f0c6d18852b461596ed6d5f6fbac29e40c6dfc
Matthieu Huin <mhu@enovance.com>
2016-02-03 17:21:53 +0100

    Add commands related to Openstack releases
    The following commands are added:
    
    - Command to check or create stable/<RELEASE> or <RELEASE>-patches branches
      on projects. The --dry-run option allows to check the state of the projects
      without creating the branches if they are missing.
    - Command to list the status of projects for a given release by matching
      data between rdoinfo, rpmfactory and the openstack/releases repo. The output
      of the command will list projects that are missing from rdo and their upstream
      repos, whether rdo projects are in rpmfactory or not, and the state of
      synchronization of the given branch with openstack's upstream.
    
    Each command can be performed on a given project, or on all projects defined in rdoinfo.
    
    Limitations:
    - the user executing the create command MUST have branch creation rights on the project(s)
    - the user's RSA key used on rpmfactory must be in ENV
    - For now existing branches that are out of sync aren't reset to the right tag.
    
    Closes-Bug: #1223
    Closes-Bug: #1236
    Related-To: #1224
    
    Change-Id: Ic9873b9f1496d873f265bcbc414d7b10fb0c3cef

---
+++ sfrdo/rdoinfoutils.py
@@ -0,0 +1,229 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+import imp
+import os
+import urlparse
+
+from rdopkg.repoman import RepoManager
+
+from sfrdo import config
+
+
+RDOINFOS_USERS_FIXES = {
+    'fpercoco@redhat.com': ('flaper87', 'flaper87@flaper87.org'),
+    'zaitcev@redhat.com': ('zaitcev', 'ota4250258078e9638@kotori.zaitcev.us'),
+    'greg.swift@rackspace.net': ('gregswift', 'xaeth@fedoraproject.org'),
+    'gchamoul@redhat.com':
+        ('strider', 'strider@rpmfactory.beta.rdoproject.org'),
+    'jprovazn@redhat.com': ('jprovaznik', 'jan.provaznik@gmail.com'),
+    'dtantsur@redhat.com': ('dtantsur', 'dtantsur@redhat.com'),
+    'dmellado@redhat.com': ('danielmellado', 'danielmelladoarea@gmail.com'),
+    'mrunge@redhat.com': ('mrunge', 'mrunge@redhat.com'),
+    'victoria@redhat.com': ('vkmc', 'victoria@redhat.com'),
+    'dprince@redhat.com': ('dprince', 'dprince@redhat.com'),
+    'nmagnezi@redhat.com': ('nmagnezi', 'nmagnezi@redhat.com'),
+    'mabaakou@redhat.com': ('sileht', 'sileht@sileht.net'),
+    'trown@redhat.com': ('trown', 'trown@redhat.com'),
+    'hguemar@redhat.com': ('hguemar', 'hguemar@fedoraproject.org'),
+    'ihrachys@redhat.com': ('booxter', 'ihrachys@redhat.com'),
+    'sferdjao@redhat.com': ('sahid', 'sahid.ferdjaoui@gmail.com'),
+    'jruzicka@redhat.com': ('yac', 'yac@rpmfactory.beta.rdoproject.org'),
+    'brad@redhat.com': ('bcrochet', 'brad@redhat.com'),
+    'slinaber@redhat.com': ('eggmaster', 'slinaber@redhat.com'),
+    'eglynn@redhat.com': ('eglynn', 'eglynn@redhat.com'),
+    'lbezdick@redhat.com': ('xbezdick', 'lbezdick@redhat.com'),
+    'msm@redhat.com': ('elmiko', 'msm@opbstudios.com'),
+    'vimartin@redhat.com': ('vkmc', 'victoria@redhat.com'),
+    'pkilambi@redhat.com': ('pkilambi', None),
+    'eharney@redhat.com': ('eharney', None),
+    'jslagle@redhat.com': ('slagle', None),
+    'matmaul@gmail.com': ('matmaul', None),
+    'egafford@redhat.com': ('egafford', None),
+    'ndipanov@redhat.com': ('djipko', None),
+    'ifarkas@redhat.com': ('ifarkas', None),
+    'zbitter@redhat.com': ('zaneb', None),
+    'apevec@redhat.com': ('apevec', None),
+    'gauvain.pocentek@objectif-libre.com': ('gpocentek', None),
+    'apevec@gmail.com': ('apavec', None),
+    'Kevin.Fox@pnnl.gov': ('kfox1111', None),
+    'majopela@redhat.com': ('mangelajo', None),
+    'marcos.fermin.lobo@cern.ch': ('marcosflobo', None),
+    'chkumar@redhat.com': ('chkumar246', None),
+    'ryansb@redhat.com': (None, None),
+    'openstack-networking@cisco.com': (None, None),
+    'jpena@redhat.com': (None, None),
+    'lennyb@mellanox.com': (None, None),
+    'brdemers@cisco.com': (None, None),
+    'ichavero@redhat.com': (None, None),
+    'mmagr@redhat.com': (None, None),
+    'xin.wu@bigswitch.com': (None, None),
+    'pbrady@redhat.com': (None, None),
+}
+
+
+RDOINFOS_FIXES = {
+    'glance_store': {
+        'distgit': 'git://pkgs.fedoraproject.org/python-glance-store.git',
+        'conf': 'client',  # Use the client style (master branch)
+    },
+    'swift': {
+        'distgit': 'git://pkgs.fedoraproject.org/openstack-swift.git',
+        'conf': 'client',  # Use the client style (master branch)
+    },
+    'horizon': {
+        'distgit': 'git://github.com/openstack-packages/horizon',
+    },
+    'dib-utils': {
+        'distgit': 'git://pkgs.fedoraproject.org/dib-utils.git',
+        'conf': 'client',  # Use the client style (master branch)
+    },
+    'tripleo-incubator': {
+        'distgit': 'git://github.com/openstack-packages/tripleo',
+        'conf': 'core',  # Use the core style rdo-liberty branch)
+        'rdo-liberty-tag': '7461b01e393931e0f4cf1ff38eadb0755a49d658',
+    },
+    'os-apply-config': {
+        'distgit': 'git://pkgs.fedoraproject.org/os-apply-config.git',
+        'conf': 'client',  # Use the client style (master branch)
+    },
+    'os-collect-config': {
+        'distgit': 'git://pkgs.fedoraproject.org/os-collect-config.git',
+        'conf': 'client',  # Use the client style (master branch)
+    },
+    'os-net-config': {
+        'distgit': 'git://pkgs.fedoraproject.org/os-net-config.git',
+        'conf': 'client',  # Use the client style (master branch)
+    },
+    'os-refresh-config': {
+        'distgit': 'git://pkgs.fedoraproject.org/os-refresh-config.git',
+        'conf': 'client',  # Use the client style (master branch)
+    },
+    'os-cloud-config': {
+        'distgit': 'git://pkgs.fedoraproject.org/os-cloud-config.git',
+        'conf': 'client',  # Use the client style (master branch)
+    },
+    'zaqar': {
+        'distgit': 'git://pkgs.fedoraproject.org/openstack-zaqar.git',
+        'conf': 'client',  # Use the client style (master branch)
+    },
+    'ironic-python-agent': {
+        'distgit':
+            'git://pkgs.fedoraproject.org/openstack-ironic-python-agent.git',
+        'conf': 'client',  # Use the client style (master branch)
+    },
+    'django_openstack_auth': {
+        'distgit':
+            'git://pkgs.fedoraproject.org/python-django-openstack-auth.git',
+    },
+    'tripleoclient': {
+        #  https://github.com/openstack-packages/python-tripleoclient/blob/rdo-liberty/python-tripleoclient.spec#L14
+        'distgit': 'git://github.com/openstack-packages/python-tripleoclient',
+        'conf': 'core',  # Use the core style rdo-liberty branch)
+        'rdo-liberty-tag': '2aac09de13f4cfd4b9d87cdcdd860388e21aef0a',
+    },
+    'openstack-puppet-modules': {
+        'distgit':
+            'git://pkgs.fedoraproject.org/openstack-puppet-modules.git',
+    },
+    'networking-arista': {  # Can be reported
+        'distgit':
+            'git://github.com/openstack-packages/python-networking-arista',
+    },
+    'horizon': {  # Can be reported
+        'conf': 'client',  # Use the core style rdo-liberty branch)
+    },
+    'tempest': {
+        'conf': 'client',
+        'distgit': 'git://pkgs.fedoraproject.org/tempest.git',
+        'rdo-liberty-tag': 'openstack-tempest-liberty-20151020',
+    },
+    'packstack': {
+        'conf': 'core',
+        'distgit': 'git://github.com/openstack-packages/packstack',
+        'rdo-liberty-tag': 'g42b3426',
+    },
+}
+
+
+def fetch_rdoinfo():
+    if not os.path.isdir(config.userdir):
+        os.mkdir(config.userdir)
+    rm = RepoManager(config.userdir, config.rdoinfo, verbose=True)
+    rm.init(force_fetch=True)
+    file, path, desc = imp.find_module('rdoinfo', [rm.repo_path])
+    rdoinfo = imp.load_module('rdoinfo', file, path, desc)
+    return rdoinfo.parse_info_file(os.path.join(config.userdir,
+                                                'rdoinfo/rdo.yml'))
+
+
+def fetch_project_infos(rdoinfo, upstream_project_name):
+    select = [pkg for pkg in rdoinfo['packages']
+              if pkg['project'] == upstream_project_name]
+    if not select:
+        # some projects differ from the upstream name (ex: oslo.* is oslo-*)
+        # so look again by using the upstream url
+        select = [pkg for pkg in rdoinfo['packages']
+                  if pkg['upstream'].endswith(upstream_project_name)]
+    if not select:
+        # yup, out of luck now    
+        raise Exception('Project not found in rdoinfo: %s' %
+                        upstream_project_name)
+    infos = select[0]
+
+    distgit = infos['distgit']
+    # Change scheme from ssh to git (avoid the need of being authenticated)
+    # For some projects we need it
+    # (eg. client project) still hosted fedora side.
+    parts = urlparse.urlparse(distgit)
+    distgit = urlparse.urlunparse(['git', parts.netloc,
+                                   parts.path, '', '', ''])
+    conf = 'None'
+    if 'conf' in infos:
+        conf = infos['conf']
+
+    if upstream_project_name in RDOINFOS_FIXES:
+        if 'distgit' in RDOINFOS_FIXES[upstream_project_name]:
+            print "Distgit target has been fixed by sfrdo !"
+            distgit = RDOINFOS_FIXES[upstream_project_name]['distgit']
+        if 'conf' in RDOINFOS_FIXES[upstream_project_name]:
+            print "Conf type has been fixed by sfrdo !"
+            conf = RDOINFOS_FIXES[upstream_project_name]['conf']
+
+    mdistgit = infos['master-distgit']
+    upstream = infos['upstream']
+    name = infos['project']
+    maints = infos['maintainers']
+    sfdistgit = "%s-distgit" % name
+    return (name, distgit, upstream,
+            sfdistgit, maints, conf, mdistgit)
+
+
+def display_details(cmdargs, rdoinfo, workdir=None):
+    name, distgit, upstream, \
+        sfdistgit, maintsi, conf, \
+        mdistgit = fetch_project_infos(rdoinfo, cmdargs.name)
+    print "=== Details ==="
+    print "Project name is: %s" % name
+    print "Project type is: %s" % conf
+    print "Project upstream RDO distgit is: %s" % distgit
+    print "Project upstream RDO master-distgit is: %s" % mdistgit
+
+    print "Project upstream is: %s" % upstream
+
+    print "Project distgit name on SF is: %s" % sfdistgit
+    print
